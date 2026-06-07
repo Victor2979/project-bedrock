585bbfcdddb70fa12aac45825f3d63eeb9356483
Victor2979 <madubuobuvictorakachukwu@gmail.com>
2026-06-07 21:15:22 +0100

    Create main.tf

---
+++ terraform/modules/vpc/main.tf
@@ -0,0 +1,24 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.8"
+
+  name = "project-bedrock-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = var.azs
+  private_subnets = [for i, az in var.azs : cidrsubnet(var.vpc_cidr, 4, i)]
+  public_subnets  = [for i, az in var.azs : cidrsubnet(var.vpc_cidr, 4, i + 4)]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = "1"
+  }
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+
+  tags = merge(var.tags, { Name = "project-bedrock-vpc" })
+}
