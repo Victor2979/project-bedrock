ca5774ba08dafd496876d41a030369cf9018e777
Victor2979 <madubuobuvictorakachukwu@gmail.com>
2026-06-07 21:31:13 +0100

    Create outputs.tf

---
+++ terraform/modules/serverless/outputs.tf
@@ -0,0 +1,3 @@
+output "assets_bucket_name" { value = aws_s3_bucket.assets.id }
+output "assets_bucket_arn"  { value = aws_s3_bucket.assets.arn }
+output "lambda_name"        { value = aws_lambda_function.processor.function_name }
