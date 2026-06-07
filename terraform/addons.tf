ded48df7c341793f06af2da2181e6dd0630c5b1b
Victor2979 <madubuobuvictorakachukwu@gmail.com>
2026-06-07 21:10:27 +0100

    Create addons.tf

---
+++ terraform/addons.tf
@@ -0,0 +1,59 @@
+# EKS access entry: map bedrock-dev-view IAM user into the "bedrock-viewers"
+# Kubernetes group, which the ClusterRoleBinding grants read-only "view".
+resource "aws_eks_access_entry" "dev_view" {
+  cluster_name      = module.eks.cluster_name
+  principal_arn     = module.iam.dev_user_arn
+  kubernetes_groups = ["bedrock-viewers"]
+  type              = "STANDARD"
+}
+
+# AWS Load Balancer Controller (for ALB ingress) via Helm + IRSA role
+module "lb_controller_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.39"
+
+  role_name                              = "bedrock-lb-controller"
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+  tags = local.common_tags
+}
+
+resource "helm_release" "lb_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.lb_controller_irsa.iam_role_arn
+  }
+  set {
+    name  = "region"
+    value = var.region
+  }
+  set {
+    name  = "vpcId"
+    value = module.vpc.vpc_id
+  }
+
+  depends_on = [module.eks]
+}
