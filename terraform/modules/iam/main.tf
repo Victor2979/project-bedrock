4166844d958ab98157882511bd025c02a049fbd4
Victor2979 <madubuobuvictorakachukwu@gmail.com>
2026-06-07 21:26:28 +0100

    Add IAM user and policies for dev view access

---
+++ terraform/modules/iam/main.tf
@@ -0,0 +1,31 @@
+resource "aws_iam_user" "dev_view" {
+  name = "bedrock-dev-view"
+  tags = var.tags
+}
+
+resource "aws_iam_user_policy_attachment" "readonly" {
+  user       = aws_iam_user.dev_view.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
+resource "aws_iam_user_policy" "s3_put" {
+  name = "bedrock-dev-s3-put"
+  user = aws_iam_user.dev_view.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["s3:PutObject"]
+      Resource = "${var.assets_bucket_arn}/*"
+    }]
+  })
+}
+
+resource "aws_iam_access_key" "dev_view" {
+  user = aws_iam_user.dev_view.name
+}
+
+resource "aws_iam_user_login_profile" "dev_view" {
+  user                    = aws_iam_user.dev_view.name
+  password_reset_required = false
+}
